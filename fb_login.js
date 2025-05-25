bb89272ae006a9e4b71fa9394d241f597a7058c7
21045sm <21045sm@hvhs.school.nz>
2025-05-25 21:37:44 +0000

    V-1 setup for login

---
+++ fb_login.js
@@ -0,0 +1,12 @@
+/***************************************************************/
+// fb_login.js
+// Term 2 / 2025
+// Samuel Moreira
+// logs the players info to firebase and saves them as a user
+// 
+// 
+/***************************************************************/
+
+function loginPage() {
+    window.location = "login.html"
+}
