70895a84e755556c66ebd6ac1d3869186f5e3212
21045sm <21045sm@hvhs.school.nz>
2025-06-16 22:40:05 +0000

    V-5 login being ported and set up, deleted a lot of the index html page code

--- JS/fb_login.js
+++ JS/fb_login.js
@@ -10,3 +10,53 @@
 function loginPage() {
     window.location = "login.html"
 }
+
+/*************************************/
+//
+//
+//
+//
+//
+/*************************************/
+function fb_login() {
+  const auth = firebase.auth();
+  console.log("logging in");
+
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      console.log("logged in");
+      console.log(user);
+      const uid = user.uid;
+      console.log(uid);
+      //fb_register()
+      //fb_saveRegistrationInfo()
+      firebase.database().ref('users/'+user.uid).set({
+        //gameName: HTML_screen_name.value,
+        googleName: user.displayName,
+        photo: user.photoURL,
+        email: user.email
+        
+      });
+      //take the user to the game
+      windowLocationGame2PMenu()
+    } else {
+      console.log("not logged in");
+      const provider = new firebase.auth.GoogleAuthProvider();
+ 
+      auth.signInWithPopup(provider)
+        .then((result) => {
+          const token = result.credential.accessToken;
+          const user = result.user;
+          console.log("User signed in:");
+          console.log(user);
+          //take the user to the game
+          windowLocationGame2PMenu()
+          //fb_register()
+          //fb_saveRegistrationInfo()
+        })
+        .catch((error) => {
+          console.error("Error during sign-in:", error);
+        });
+    }
+  });
+}
