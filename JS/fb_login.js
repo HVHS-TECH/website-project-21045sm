653627d2f13da842b93e83ff044d229bda814072
21045sm <21045sm@hvhs.school.nz>
2025-07-14 22:32:51 +0000

    V-5.2 commented out a ton of code related to logging in and firebase and all that, not needed for now and might be deleted later.

--- JS/fb_login.js
+++ JS/fb_login.js
@@ -19,40 +19,40 @@ function loginPage() {
 //Output: user is logged into the website with their account 
 //and their info is saved to firebase
 /*************************************/
-function fb_login() {
-  const auth = firebase.auth();
-  console.log("logging in");
-
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      console.log("logged in");
-      console.log(user);
-      const uid = user.uid;
-      console.log(uid);
-      firebase.database().ref('users/'+user.uid).set({
-        googleName: user.displayName,
-        photo: user.photoURL,
-        email: user.email
-        
-      });
-      //take the user to the game
-      windowLocationGame2PMenu()
-    } else {
-      console.log("not logged in");
-      const provider = new firebase.auth.GoogleAuthProvider();
- 
-      auth.signInWithPopup(provider)
-        .then((result) => {
-          const token = result.credential.accessToken;
-          const user = result.user;
-          console.log("User signed in:");
-          console.log(user);
-          //take the user to the game
-          windowLocationGame2PMenu()
-        })
-        .catch((error) => {
-          console.error("Error during sign-in:", error);
-        });
-    }
-  });
-}
+//function fb_login() {
+//  const auth = firebase.auth();
+//  console.log("logging in");
+//
+//  auth.onAuthStateChanged((user) => {
+//    if (user) {
+//      console.log("logged in");
+//      console.log(user);
+//      const uid = user.uid;
+//      console.log(uid);
+//      firebase.database().ref('users/'+user.uid).set({
+//        googleName: user.displayName,
+//        photo: user.photoURL,
+//        email: user.email
+//        
+//      });
+//      //take the user to the game
+//      windowLocationGame2PMenu()
+//    } else {
+//      console.log("not logged in");
+//      const provider = new firebase.auth.GoogleAuthProvider();
+// 
+//      auth.signInWithPopup(provider)
+//        .then((result) => {
+//          const token = result.credential.accessToken;
+//          const user = result.user;
+//          console.log("User signed in:");
+//          console.log(user);
+//          //take the user to the game
+//          windowLocationGame2PMenu()
+//        })
+//        .catch((error) => {
+//          console.error("Error during sign-in:", error);
+//        });
+//    }
+//  });
+//}
---
+++ JS/menu.js
@@ -0,0 +1,26 @@
+/***************************************************************/
+// menu.js
+// Term 2 / 2025
+// Samuel Moreira
+// the menu page for the games, and other pages
+// 
+// 
+/***************************************************************/
+
+function menuPage() {
+    window.location = "HTML/menu.html";
+}
+
+/*************************************/
+//menuPage
+//takes the user to the menu page and runs the menu
+//called by login.html
+//Input: login button clicked - automatically takes you to the menu
+//Output: user is on the menu page with all the games
+/*************************************/
+function menuPage() {
+//  const auth = firebase.auth();
+  console.log("menu page");
+  window.location = "HTML/menu.html";
+  
+}
